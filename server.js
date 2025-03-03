9c8d4f01467c9181b49ec047fffbbdb99ab371fe
Hasan Özdemir <develophasan@gmail.com>
2025-03-03 08:38:33 +0300

    Firebase config moved to environment variables

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const { getDatabase, ref, push, get, query, orderByChild } = require('firebase/d
 dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpyWxrM6jxOxEUEbb6VkV5eMaviv6fBaQ",
-  authDomain: "piwa-ec527.firebaseapp.com",
-  databaseURL: "https://piwa-ec527-default-rtdb.firebaseio.com",
-  projectId: "piwa-ec527",
-  storageBucket: "piwa-ec527.firebasestorage.app",
-  messagingSenderId: "987555046428",
-  appId: "1:987555046428:web:486e6476728d0ae12f6bd9",
-  measurementId: "G-F4WG6VHXXW"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Firebase başlatma
--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpyWxrM6jxOxEUEbb6VkV5eMaviv6fBaQ",
-  authDomain: "piwa-ec527.firebaseapp.com",
-  databaseURL: "https://piwa-ec527-default-rtdb.firebaseio.com",
-  projectId: "piwa-ec527",
-  storageBucket: "piwa-ec527.firebasestorage.app",
-  messagingSenderId: "987555046428",
-  appId: "1:987555046428:web:486e6476728d0ae12f6bd9",
-  measurementId: "G-F4WG6VHXXW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
